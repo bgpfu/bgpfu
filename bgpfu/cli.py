37b9e76a9fc2ce1f54ab8b045f431ce0611b7155
Matt Griswold <grizz@20c.com>
2016-06-23 03:04:59 -0500

    add output options

--- bgpfu/cli.py
+++ bgpfu/cli.py
@@ -6,12 +6,24 @@ import bgpfu.client
 import logging
 
 
+def get_output_formats():
+    # TODO pkg_resource ls and plugins?
+    return ('cisco_ios', 'cisco_iox', 'juniper', 'txt')
+
+
 def common_options(f):
     f = click.option('--debug', is_flag=True, default=False)(f)
     return f
 
+
 def as_set_options(f):
     f = click.option('--skip-as', help='comma separated list of ASNs to not include in results', default=False)(f)
+    return f
+
+
+def output_options(f):
+    f = click.option('--output-format', '-f', help='output format', type=click.Choice(get_output_formats()), default='txt', show_default=True)(f)
+    f = click.option('--output', '-o', help='output file', default='-')(f)
     return f
 
 
@@ -58,13 +70,14 @@ def as_set(ctx, as_set, **kwargs):
 @cli.command()
 #@connect_options
 @common_options
+@output_options
 @click.option('--aggregate/--no-aggregate', '-A', help='aggregate prefixes', is_flag=True, default=True)
 @click.option('--sources', help='use only specified sources (default is all)')
 @click.option('--4', '-4', 'proto', help="use IPv4", flag_value='4', default=True)
 @click.option('--6', '-6', 'proto', help='use IPv6', flag_value='6')
 @click.argument('as-set', nargs=-1)
 @click.pass_context
-def prefixlist(ctx, as_set, proto, **kwargs):
+def prefixlist(ctx, as_set, output, proto, **kwargs):
     """ get prefix list for specified as-sets """
     if kwargs.get('debug', False):
         logging.basicConfig(level=logging.DEBUG)
@@ -79,5 +92,8 @@ def prefixlist(ctx, as_set, proto, **kwargs):
         if kwargs.get('aggregate', True):
             prefixes = prefixes.aggregate()
 
-        print("\n".join(prefixes.str_list()))
+        with click.open_file(output, 'w') as fobj:
+            if kwargs['output_format'] == 'txt':
+                fobj.write('\n'.join(prefixes.str_list()))
+                fobj.write('\n')
 
