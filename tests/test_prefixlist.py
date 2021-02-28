a2325f54f67de2971a0daa864def955b0f3c91f6
Matt Griswold <grizz@20c.com>
2021-02-28 07:27:10 -0600

    black tests

--- tests/test_prefixlist.py
+++ tests/test_prefixlist.py
@@ -50,7 +50,6 @@ def test_prefixlist_eq():
     pfx1.pop()
     assert pfx0 != pfx1
 
-
     with pytest.raises(TypeError) as excinfo:
         pfx0 != "string"
     assert "object not PrefixList type" in str(excinfo.value)
